6e02c08c35220f18d9332f11e98035492a0e1426
Matt Pietal <matthew.pietal@gmail.com>
2015-02-02 16:45:33 -0500

    Initial commit

---
+++ desktop/src/com/railwaygames/sleddingsmash/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.railwaygames.sleddingsmash.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.railwaygames.sleddingsmash.SleddingSmash;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SleddingSmash(), config);
+	}
+}
