defed784676a1ca5bffc19f60831c5dbc917d976
Conor Mullen <conor.mullen@gmail.com>
2015-03-14 18:01:18 -0400

    Dockerfile for jenkins build. Fix android classFix iOS base class

--- ios/src/com/railwaygames/sleddingsmash/IOSLauncher.java
+++ ios/src/com/railwaygames/sleddingsmash/IOSLauncher.java
@@ -5,13 +5,12 @@ import org.robovm.apple.uikit.UIApplication;
 
 import com.badlogic.gdx.backends.iosrobovm.IOSApplication;
 import com.badlogic.gdx.backends.iosrobovm.IOSApplicationConfiguration;
-import com.railwaygames.sleddingsmash.SleddingSmash;
 
 public class IOSLauncher extends IOSApplication.Delegate {
     @Override
     protected IOSApplication createApplication() {
         IOSApplicationConfiguration config = new IOSApplicationConfiguration();
-        return new IOSApplication(new SleddingSmash(), config);
+        return new IOSApplication(new GameLoop(), config);
     }
 
     public static void main(String[] argv) {
