54896cdb8c5e79907de71833622d990f41a07966
Conor Mullen <conor.mullen@gmail.com>
2015-03-16 06:56:52 -0400

    Signing script and Dockerfile include setup for keystore

--- android/res/sign.sh
+++ android/res/sign.sh
@@ -3,5 +3,4 @@ jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
 -keystore $JENKINS_HOME/sleddingSmash.keystore android/build/outputs/apk/android-release-unsigned.apk alias_name \
 -keypass railwaygames113 -storepass railwaygames113
 
-
 $ANDROID_HOME/build-tools/20.0.0/zipalign -v 4 android/build/outputs/apk/android-release-unsigned.apk sleddingSmash.apk
