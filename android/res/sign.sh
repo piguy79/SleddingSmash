dd94fb6292060094c17a0cd9b57bd9d4ec5549d2
Conor Mullen <conor.mullen@gmail.com>
2015-03-16 06:50:15 -0400

    Signing script

---
+++ android/res/sign.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+jarsigner -verbose -sigalg SHA1withRSA -digestalg SHA1 \
+-keystore $JENKINS_HOME/sleddingSmash.keystore android/build/outputs/apk/android-release-unsigned.apk alias_name \
+-keypass railwaygames113 -storepass railwaygames113
+
+
+$ANDROID_HOME/build-tools/20.0.0/zipalign -v 4 android/build/outputs/apk/android-release-unsigned.apk sleddingSmash.apk
